df44ed5de94a05d650b579411707dc655d8995d1
Cristian Gallucci <cristiangallucci11@gmail.com>
2025-03-17 15:09:21 +0100

    Reset del merge #1

--- Assets/_Scripts/KeyGate.cs
+++ Assets/_Scripts/KeyGate.cs
@@ -4,10 +4,10 @@ public class KeyGate : MonoBehaviour
 {
     int keyPos = 0; //impostare in base alla posizione dell'item key nell'array di PickUp
     private void OnTriggerEnter(Collider other)
-    {/*
+    {
         if (other.name == "Player" && other.GetComponent<PickUp>().objectNumber[keyPos] != 0)
         {
             //play animation
-        }*/
+        }
     }
 }
--- Assets/_Scripts/PickUp.cs
+++ Assets/_Scripts/PickUp.cs
@@ -1,17 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickUp : MonoBehaviour
 {
-    public Item item = new Item("Item Name", 1);
 
-    void OnTriggerEnter(Collider other)
+
+    string[] objectName = new string[] {};
+    public Text[] objectText = new Text[] { };
+    public int[] objectNumber = new int[] { 0, 0, 0 };
+    
+
+    
+
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        int n = 0;
+        //illumina tasto
+        //is tasto premuto
+        foreach (string a in objectName)
         {
-            Inventory.instance.AddItem(item);
-            Destroy(gameObject);
+            if (other.name == objectName[n])
+            {
+                objectNumber[n] += other.GetComponent<Amount>().amount;
+                objectText[n].text = objectNumber[n].ToString();
+            }
+            else n++;
         }
     }
 }
